a41c52410262f2e34509374b58bc5c6bcb2b4119
bouchja1 <jan.bouchner@gmail.com>
2014-04-15 12:45:39 +0200

    configurable cores

--- src/main/java/cz/cvut/fit/bouchja1/mi_dip/rest/client/solr/SolrService.java
+++ src/main/java/cz/cvut/fit/bouchja1/mi_dip/rest/client/solr/SolrService.java
@@ -6,7 +6,9 @@ package cz.cvut.fit.bouchja1.mi_dip.rest.client.solr;
 
 import cz.cvut.fit.bouchja1.mi_dip.rest.client.dao.ArticleDao;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import javax.annotation.PostConstruct;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,7 @@ public class SolrService {
     
     private String serverUrl;
     private Map<String, HttpSolrServer> validServers = new HashMap<String, HttpSolrServer>();
+    private Set<String> validSolrCores;
     
     //DAO
     @Autowired
@@ -30,7 +33,11 @@ public class SolrService {
     
     @PostConstruct
     public void createValidSolrServers() {
-        validServers.put("collection1", new HttpSolrServer(serverUrl + "collection1"));
+        Iterator<String> validCores = validSolrCores.iterator();
+        while (validCores.hasNext()) {
+            String core = validCores.next();
+            validServers.put(core, new HttpSolrServer(serverUrl + core));
+        }        
     }
 
     public String getServerUrl() {
@@ -39,5 +46,11 @@ public class SolrService {
 
     public void setServerUrl(String serverUrl) {
         this.serverUrl = serverUrl;
-    }            
+    }    
+
+    public void setValidSolrCores(Set<String> validSolrCores) {
+        this.validSolrCores = validSolrCores;
+    }
+    
+    
 }
